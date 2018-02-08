b21b9a256e2332d1ea59e1ebe5f08ee45c221342
idubnori <i.dub.nori@gmail.com>
2018-02-08 07:06:37 -0500

    Added XML comments into data models.

--- src/TrackInfoReader/Models/Artwork.cs
+++ src/TrackInfoReader/Models/Artwork.cs
@@ -1,7 +1,14 @@
 ﻿namespace ShairportSync.Metadata.Models
 {
+    /// <summary>
+    /// Artwork instance creates by reading each artwork image metadata item.
+    /// </summary>
     public class Artwork : TrackInfo
     {
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="trackInfo">Source track information.</param>
         public Artwork(TrackInfo trackInfo) : base(trackInfo) { }
     }
 }
--- src/TrackInfoReader/Models/TrackInfo.cs
+++ src/TrackInfoReader/Models/TrackInfo.cs
@@ -2,12 +2,22 @@
 
 namespace ShairportSync.Metadata.Models
 {
+    /// <summary>
+    /// Track information which parsed shairport-sync metadata.
+    /// </summary>
     public class TrackInfo
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public TrackInfo()
         {
         }
 
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source">Copy source.</param>
         public TrackInfo(TrackInfo source)
         {
             PlaybackDateTime = source.PlaybackDateTime;
@@ -18,11 +28,34 @@ namespace ShairportSync.Metadata.Models
             Artwork = source.Artwork;
         }
 
+        /// <summary>
+        /// Track Playback DateTime. 
+        /// </summary>
         public DateTime PlaybackDateTime { get; set; }
+
+        /// <summary>
+        /// DMAP Persistent Id.
+        /// </summary>
         public string Mper { get; set; }
+
+        /// <summary>
+        /// Artist Name.
+        /// </summary>
         public string Artist { get; set; }
+
+        /// <summary>
+        /// Album Name.
+        /// </summary>
         public string Album { get; set; }
+
+        /// <summary>
+        /// Song name.
+        /// </summary>
         public string Song { get; set; }
+
+        /// <summary>
+        /// Artwork Image File Name. (as temporal)
+        /// </summary>
         public string Artwork { get; set; }
     }
 }
